dde38619fabd71ae871319cf546edb163eeb63cc
Eric Mariot <ericmariots@gmail.com>
2020-09-08 10:55:46 -0300

    readme

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,86 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+interface SignInFormData {
+  email: string,
+  password: string,
+}
+
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
+
+  const  handleSubmit = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string().required('A valid e-mail is required ').email('Type a valid e-mail'),
+        password: Yup.string().required('Password required'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await signIn({
+        email: data.email,
+        password: data.password,
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError){
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
+
+      // disparar um toast
+      addToast();
+    }
+  }, [signIn, addToast]);
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber"/>
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Make your login</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+
+          <Button type="submit">Login</Button>
+
+          <a href="forgot">Forgot my password</a>
+        </Form>
+
+        <a href="login">
+          <FiLogIn />
+          Create account
+        </a>
+      </Content>
+
+      <Background />
+
+    </Container>
+  );
+}
+
+export default SignIn;

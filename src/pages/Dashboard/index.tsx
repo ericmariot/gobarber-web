35081e7901eb78d2e732b937f150310759bfcab1
Eric Mariot <ericmariots@gmail.com>
2020-11-17 14:24:30 -0300

    first part

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
+
+import logoImg from '../../assets/logo.svg';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber"/>
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name}/>
+            <div>
+              <span>Welcome,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower/>
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;

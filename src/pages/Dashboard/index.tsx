20d74c25abd596a13fd67a03c4b15a92d6ae1e95
Eric Mariot <ericmariots@gmail.com>
2020-11-19 14:14:04 -0300

    appointment list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Section, Appointment, Calendar } from './styles';
 import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -41,7 +43,7 @@ const Dashboard: React.FC = () => {
           <NextAppointment>
             <strong>Next appointment</strong>
             <div>
-              <img src="https://leaguefeed.net/wp-content/uploads/2020/10/rarest-league-of-legends-icons.jpg" alt="Imagem Qualquer"/>
+              <img src="https://leaguefeed.net/wp-content/uploads/2020/10/rarest-league-of-legends-icons.jpg" alt="Nome da pessoa"/>
 
               <strong>Eric Mariot</strong>
               <span>
@@ -50,7 +52,52 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Morning</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://leaguefeed.net/wp-content/uploads/2020/10/rarest-league-of-legends-icons.jpg" alt="Nome da pessoa"/>
+                <strong>Eric Mariot</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://leaguefeed.net/wp-content/uploads/2020/10/rarest-league-of-legends-icons.jpg" alt="Nome da pessoa"/>
+                <strong>Eric Mariot</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Afternoon</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://leaguefeed.net/wp-content/uploads/2020/10/rarest-league-of-legends-icons.jpg" alt="Nome da pessoa"/>
+                <strong>Eric Mariot</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
+
         <Calendar/>
       </Content>
     </Container>
